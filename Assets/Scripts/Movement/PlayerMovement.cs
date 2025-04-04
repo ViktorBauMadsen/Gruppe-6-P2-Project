955ccacf93e28c0ba9da552dea1e29ab9eb9f512
Viktor <121439486+ViktorBauMadsen@users.noreply.github.com>
2025-04-04 13:36:28 +0200

    removed useless line of code from movement script

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -25,25 +25,20 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
-        // Set initial rotation of orientation to 90 degrees on the Y-axis
-        if (orientation != null)
-        {
-            orientation.rotation = Quaternion.Euler(0, 90, 0);
-        }
+        
     }
 
     private void Update()
     {
         // ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-
+        
         MyInput();
 
         // handle drag
         if (grounded)
             rb.linearDamping = groundDrag;
-        else
+        else 
             rb.linearDamping = 0;
     }
 
@@ -64,4 +59,13 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
+
+
+
+
+
+
+
+
 }
+    
