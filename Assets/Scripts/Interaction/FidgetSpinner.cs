ef3af018fe6a570c00612d721a4be32bde37ff4a
Matz20 <matthewvictory12@gmail.com>
2025-04-09 14:35:31 +0200

    working and fixed

--- Assets/Scripts/Interaction/FidgetSpinner.cs
+++ Assets/Scripts/Interaction/FidgetSpinner.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FidgetSpinner : MonoBehaviour, IInteractable
@@ -6,22 +7,24 @@ public class FidgetSpinner : MonoBehaviour, IInteractable
     private ObjectiveManager objectiveManager;
 
     private void Start()
-{
-    animator = GetComponent<Animator>();
-       objectiveManager = FindObjectOfType<ObjectiveManager>();
+    {
+        animator = GetComponent<Animator>();
+        objectiveManager = FindObjectOfType<ObjectiveManager>();
 
     }
 
     public string GetDescription()
-{
+    {
         return "To spin";
-}
+    }
 
-public void Interact()
-{
-    animator.SetTrigger("Spin");
+    public void Interact()
+    {
+        animator.SetTrigger("Spin");
         Debug.Log("Fidget spinner interacted with.");
         objectiveManager.CompleteCurrentObjective();
+
+        StressValueHolder.singleton.RemoveStress(15);
 
     }
 }
