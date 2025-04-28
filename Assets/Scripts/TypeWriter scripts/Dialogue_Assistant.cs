58e392a08679395b753fc9d622956d830945d1c7
Viktor <121439486+ViktorBauMadsen@users.noreply.github.com>
2025-04-28 17:58:18 +0200

    Undertale
    Made TypeWriter (Undertale) text for quiz scenes

---
+++ Assets/Scripts/TypeWriter scripts/Dialogue_Assistant.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class Dialogue_Assistant : MonoBehaviour
+{
+    [SerializeField] private TextWriter TextWriter;
+    private TextMeshProUGUI messageText;
+    private void Awake()
+    {
+        if (messageText == null)
+        {
+            messageText = transform.Find("message").Find("messageText").GetComponent<TextMeshProUGUI>();
+        }
+    }
+
+    private void Start()
+    {
+        string originalText = messageText.text;
+        if (messageText != null)
+        {
+            originalText = messageText.text;
+            messageText.text = string.Empty;
+        }
+
+        if(TextWriter!=null && messageText != null)
+        {
+            TextWriter.AddWriter(messageText, originalText, 0.03f);
+        }
+        
+
+    }
+
+
+
+
+
+}
